2cb396f9ff7306f87463bf110c561603cd7653d6
Rocky Chen <jinweizsu@gmail.com>
2024-03-06 23:12:00 +0800

    update

--- src/main.js
+++ src/main.js
@@ -1,25 +1,18 @@
-require('dotenv').config()
-const Discord = require('discord.js');
-const { Intents } = require('discord.js');
-const client = new Discord.Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+// Initialize dotenv
+require('dotenv').config();
 
-// 定义事件：当Bot完成登录并准备好时触发
+// Discord.js versions ^13.0 require us to explicitly define client intents
+const { Client, Intents } = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}`);
+ console.log(`Logged in as ${client.user.tag}!`);
 });
 
+// Log In our bot
+// client.login(process.env.CLIENT_TOKEN);
 
-
-// 定义事件：当收到消息时触发
-client.on('messageCreate', (message) => {
-  // 检查消息是否提及Bot
-  if (message.mentions.has(client.user)) {
-    console.log(`${message.author.username} mentioned me: ${message.content}`);
-  }else{
-    console.log(`${message.author.username} said: ${message.content}`);
-  }
-});
-
+// client.login(TOKEN);
 
 
 // 运行Bot
