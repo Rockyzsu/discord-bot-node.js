68d189e7e7cec5fab88222b903735f1450ed69b1
Rocky Chen <jinweizsu@gmail.com>
2024-03-07 00:14:07 +0800

    update

--- src/main.js
+++ src/main.js
@@ -1,27 +1,24 @@
 // Initialize dotenv
-require('dotenv').config();
+require("dotenv").config();
 
 // Discord.js versions ^13.0 require us to explicitly define client intents
-const { Client, GatewayIntentBits } = require('discord.js');
+const { Client, GatewayIntentBits } = require("discord.js");
 const client = new Client({
-  intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMessages,
-  ],
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages],
 });
 
-client.on('messageCreate', (message) => {
-    console.log(`👍 works as expected`);
-  });
-  client.on('interactionCreate', (interaction) => {
-    console.log(`👍 works as expected`);
-  });
+client.on("messageCreate", (message) => {
+  console.log(`👍 works as expected`);
+});
+client.on("interactionCreate", (interaction) => {
+  console.log(`👍 works as expected`);
+});
 
 // Log In our bot
 // client.login(process.env.CLIENT_TOKEN);
 
 // client.login(TOKEN);
 
-
 // 运行Bot
+console.log(process.env.TOKEN)
 client.login(process.env.TOKEN); // 替换为您的Discord令牌
